626884bcee28271cce8883a8eec582cc29226ffa
zihanla <2622293253@qq.com>
2023-05-29 22:46:47 +0800

    day02

---
+++ src/05.函数组的创建和渲染.jsx
@@ -0,0 +1,13 @@
+function Hello() {
+    return <h2>zihan</h2>
+}
+
+function App() {
+    return (
+        <div className="App">
+            <Hello/>
+        </div>
+    );
+}
+
+export default App;
